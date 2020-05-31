0ea7627609961657bc3437c49fcb67e86faafb8b
srmchartroom <sean@chartroomcreative.com>
2020-05-30 22:13:52 -0400

    Create script.js, initially set up script file to query for and dynamically set quiz title span

---
+++ script.js
@@ -0,0 +1,4 @@
+// access the span with id of #quizTitle
+var quizTitle = document.querySelector("#quizTitle");
+// dynamically add quiz title
+quizTitle.textContent = "Javascript Basics - Level I.";
