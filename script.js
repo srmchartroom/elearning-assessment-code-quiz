4ea8b3db9ac6d356975594dc1484053eb3fd93e7
srmchartroom <sean@chartroomcreative.com>
2020-06-01 17:44:35 -0400

    Added event listeners to buttons. Added styles.css and added .hidden and .shown classes to handle visibility toggling of different quiz question divs.

--- script.js
+++ script.js
@@ -9,20 +9,20 @@ quizTitle.textContent = "Javascript Basics - Level I.";
 // Get High Scores Link set to goToScoresBtn
 const goToScoresBtn = document.querySelector("#goToScores");
 //! -- TIME REMAINING TICKER -- //
-// Get time remaining span and set to spanTimeRemEle
-const spanTimeEle = document.querySelector("#timer");
+// Get time remaining span and set to spanTimeRem
+const spanTime = document.querySelector("#timer");
 
 //! QUIZ ELEMENTS
 
 //! -- QZ0 ELEMENTS -- //
-// get qz0 div and set it to qz0DivEle
-const qz0DivEle = document.querySelector("#qz0");
+// get qz0 div and set it to qz0Div
+const qz0Div = document.querySelector("#qz0");
 // get Start Quiz Button and set it to startBtn 
 const startBtn = document.querySelector("#startBtn");
 
 //! -- QZ1 ELEMENTS -- //
-// get qz1 div and set it to qz1DivEle
-const qz1DivEle = document.querySelector("#qz1");
+// get qz1 div and set it to qz1Div
+const qz1Div = document.querySelector("#qz1");
 // get value for question 1A and set to q1A
 const q1A = document.querySelector("#question1A");
 // get value for question 1B and set to q1B
@@ -35,14 +35,18 @@ const q1D = document.querySelector("#question1D");
 const q1E = document.querySelector("#question1E");
 // get "Next" button for QZ1 and set to nxt1Btn
 const nxt1Btn = document.querySelector("#nxt1Btn");
+
+// Add event listener to nxt1Btn button
+nxt1Btn.addEventListener("click", qz1Next);
+
 // get correct message alert and set to q1CorrectMsg
 const q1CorrectMsg = document.querySelector("#q1CorrectMsg");
 // get incorrect message alert and set to q1IncorrectMsg
 const q1IncorrectMsg = document.querySelector("#q1IncorrectMsg");
 
 //! -- QZ2 ELEMENTS -- //
-// get qz2 div and set it to qz2DivEle
-const qz2DivEle = document.querySelector("#qz2");
+// get qz2 div and set it to qz2Div
+const qz2Div = document.querySelector("#qz2");
 // get value for question 2A and set to q2A
 const q2A = document.querySelector("#question2A");
 // get value for question 2B and set to q2B
@@ -55,14 +59,18 @@ const q2D = document.querySelector("#question2D");
 const q2E = document.querySelector("#question2E");
 // get "Next" button for QZ2 and set to nxt2Btn
 const nxt2Btn = document.querySelector("#nxt2Btn");
+
+// Add event listener to nxt1Btn button
+nxt2Btn.addEventListener("click", qz2Next);
+
 // get correct message alert and set to q2CorrectMsg
 const q2CorrectMsg = document.querySelector("#q2CorrectMsg");
 // get incorrect message alert and set to q2IncorrectMsg
 const q2IncorrectMsg = document.querySelector("#q2IncorrectMsg");
 
 //! -- QZ3 ELEMENTS -- //
-// get qz3 div and set it to qz3DivEle
-const qz3DivEle = document.querySelector("#qz3");
+// get qz3 div and set it to qz3Div
+const qz3Div = document.querySelector("#qz3");
 // get value for question 3A and set to q3A
 const q3A = document.querySelector("#question3A");
 // get value for question 3B and set to q3B
@@ -75,14 +83,18 @@ const q3D = document.querySelector("#question3D");
 const q3E = document.querySelector("#question3E");
 // get "Next" button for QZ3 and set to nxt3Btn
 const nxt3Btn = document.querySelector("#nxt3Btn");
+
+// Add event listener to nxt1Btn button
+nxt3Btn.addEventListener("click", qz3Next);
+
 // get correct message alert and set to q3CorrectMsg
 const q3CorrectMsg = document.querySelector("#q3CorrectMsg");
 // get incorrect message alert and set to q3IncorrectMsg
 const q3IncorrectMsg = document.querySelector("#q3IncorrectMsg");
 
 //! -- QZ4 ELEMENTS -- //
-// get qz4 div and set it to qz4DivEle
-const qz4DivEle = document.querySelector("#qz4");
+// get qz4 div and set it to qz4Div
+const qz4Div = document.querySelector("#qz4");
 // get value for question 4A and set to q4A
 const q4A = document.querySelector("#question4A");
 // get value for question 4B and set to q4B
@@ -95,14 +107,19 @@ const q4D = document.querySelector("#question4D");
 const q4E = document.querySelector("#question4E");
 // get "Next" button for QZ4 and set to nxt4Btn
 const nxt4Btn = document.querySelector("#nxt4Btn");
+
+// Add event listener to nxt1Btn button
+nxt4Btn.addEventListener("click", qz4Next);
+
 // get correct message alert and set to q4CorrectMsg
 const q4CorrectMsg = document.querySelector("#q4CorrectMsg");
 // get incorrect message alert and set to q4IncorrectMsg
 const q4IncorrectMsg = document.querySelector("#q4IncorrectMsg");
 
+
 //! -- QZ5 ELEMENTS -- //
-// get qz5 div and set it to qz5DivEle
-const qz5DivEle = document.querySelector("#qz5");
+// get qz5 div and set it to qz5Div
+const qz5Div = document.querySelector("#qz5");
 // get value for question 5A and set to q5A
 const q5A = document.querySelector("#question5A");
 // get value for question 5B and set to q5B
@@ -115,14 +132,18 @@ const q5D = document.querySelector("#question5D");
 const q5E = document.querySelector("#question5E");
 // get "Next" button for QZ5 and set to nxt5Btn
 const nxt5Btn = document.querySelector("#nxt5Btn");
+
+// Add event listener to nxt1Btn button
+nxt5Btn.addEventListener("click", qz5Next);
+
 // get correct message alert and set to q5CorrectMsg
 const q5CorrectMsg = document.querySelector("#q5CorrectMsg");
 // get incorrect message alert and set to q5IncorrectMsg
 const q5IncorrectMsg = document.querySelector("#q5IncorrectMsg");
 
 //! -- QZ6 ELEMENTS -- //
-// get qz6 div and set it to qz6DivEle
-const qz6DivEle = document.querySelector("#qz6");
+// get qz6 div and set it to qz6Div
+const qz6Div = document.querySelector("#qz6");
 // get value for question 6A and set to q6A
 const q6A = document.querySelector("#question6A");
 // get value for question 6B and set to q6B
@@ -135,14 +156,18 @@ const q6D = document.querySelector("#question6D");
 const q6E = document.querySelector("#question6E");
 // get "Next" button for QZ6 and set to nxt6Btn
 const nxt6Btn = document.querySelector("#nxt6Btn");
+
+// Add event listener to nxt1Btn button
+nxt6Btn.addEventListener("click", qz6Next);
+
 // get correct message alert and set to q6CorrectMsg
 const q6CorrectMsg = document.querySelector("#q6CorrectMsg");
 // get incorrect message alert and set to q6IncorrectMsg
 const q6IncorrectMsg = document.querySelector("#q6IncorrectMsg");
 
 //! -- QZ7 ELEMENTS -- //
-// get qz7 div and set it to qz7DivEle
-const qz7DivEle = document.querySelector("#qz7");
+// get qz7 div and set it to qz7Div
+const qz7Div = document.querySelector("#qz7");
 // get value for question 7A and set to q7A
 const q7A = document.querySelector("#question7A");
 // get value for question 7B and set to q7B
@@ -155,14 +180,18 @@ const q7D = document.querySelector("#question7D");
 const q7E = document.querySelector("#question7E");
 // get "Next" button for QZ7 and set to nxt7Btn
 const nxt7Btn = document.querySelector("#nxt7Btn");
+
+// Add event listener to nxt1Btn button
+nxt7Btn.addEventListener("click", qz7Next);
+
 // get correct message alert and set to q7CorrectMsg
 const q7CorrectMsg = document.querySelector("#q7CorrectMsg");
 // get incorrect message alert and set to q7IncorrectMsg
 const q7IncorrectMsg = document.querySelector("#q7IncorrectMsg");
 
 //! -- QZ8 ELEMENTS -- //
-// get qz8 div and set it to qz8DivEle
-const qz8DivEle = document.querySelector("#qz8");
+// get qz8 div and set it to qz8Div
+const qz8Div = document.querySelector("#qz8");
 // get value for question 8A and set to q8A
 const q8A = document.querySelector("#question8A");
 // get value for question 8B and set to q8B
@@ -175,14 +204,18 @@ const q8D = document.querySelector("#question8D");
 const q8E = document.querySelector("#question8E");
 // get "Next" button for QZ8 and set to nxt8Btn
 const nxt8Btn = document.querySelector("#nxt8Btn");
+
+// Add event listener to nxt1Btn button
+nxt8Btn.addEventListener("click", qz8Next);
+
 // get correct message alert and set to q8CorrectMsg
 const q8CorrectMsg = document.querySelector("#q8CorrectMsg");
 // get incorrect message alert and set to q8IncorrectMsg
 const q8IncorrectMsg = document.querySelector("#q8IncorrectMsg");
 
 //! -- QZ9 ELEMENTS -- //
-// get qz9 div and set it to qz9DivEle
-const qz9DivEle = document.querySelector("#qz9");
+// get qz9 div and set it to qz9Div
+const qz9Div = document.querySelector("#qz9");
 // get value for question 9A and set to q9A
 const q9A = document.querySelector("#question9A");
 // get value for question 9B and set to q9B
@@ -195,14 +228,18 @@ const q9D = document.querySelector("#question9D");
 const q9E = document.querySelector("#question9E");
 // get "Next" button for QZ9 and set to nxt9Btn
 const nxt9Btn = document.querySelector("#nxt9Btn");
+
+// Add event listener to nxt1Btn button
+nxt9Btn.addEventListener("click", qz9Next);
+
 // get correct message alert and set to q9CorrectMsg
 const q9CorrectMsg = document.querySelector("#q9CorrectMsg");
 // get incorrect message alert and set to q9IncorrectMsg
 const q9IncorrectMsg = document.querySelector("#q9IncorrectMsg");
 
 //! -- QZ10 ELEMENTS -- //
-// get qz10 div and set it to qz10DivEle
-const qz10DivEle = document.querySelector("#qz10");
+// get qz10 div and set it to qz10Div
+const qz10Div = document.querySelector("#qz10");
 // get value for question 10A and set to q10A
 const q10A = document.querySelector("#question10A");
 // get value for question 10B and set to q10B
@@ -216,19 +253,27 @@ const q10E = document.querySelector("#question10E");
 // get "Next" button for QZ10 and set to nxt10Btn
 const nxt10Btn = document.querySelector("#nxt10Btn");
 // get correct message alert and set to q10CorrectMsg
+
+// Add event listener to nxt1Btn button
+nxt1Btn.addEventListener("click", finishAndScore);
+
 const q10CorrectMsg = document.querySelector("#q10CorrectMsg");
 // get incorrect message alert and set to q10IncorrectMsg
 const q10IncorrectMsg = document.querySelector("#q10IncorrectMsg");
 
 //! -- QZ11 (Scoring Conclusion) ELEMENTS -- //
-// get qz11 div and set it to qz11DivEle
-const qz11DivEle = document.querySelector("#qz11");
+// get qz11 div and set it to qz11Div
+const qz11Div = document.querySelector("#qz11");
 // get qz11 number correct span and set to spanNumCorrect
 const spanNumCorrect = document.querySelector("#numCorrect");
 // get qz11 final weighted score span and set to spanFinalScore
 const spanFinalScore = document.querySelector("#score");
 // get qz11 retake button and set to qz11retakeBtn
 const qz11retakeBtn = document.querySelector("#retakeBtn");
+
+// Add event listener to nxt1Btn button
+qz11retakeBtn.addEventListener("click", retakeQuiz);
+
 // get initials input field and set to initialsInput
 const initialsInput = document.querySelector("#initialsInput");
 // get submit score button and set to scoreItBtn
@@ -243,8 +288,17 @@ const scoresTable = document.querySelector("#scoresTable");
 const scoresTableBody = document.querySelector("#scoresTableBody");
 // get the qz12/scores retake btn and set it to scoresRetakeBtn
 const scoresRetakeBtn = document.querySelector("#retake2Btn");
+
+// Add event listener to nxt1Btn button
+scoresRetakeBtn.addEventListener("click", retakeQuiz);
+
 // get the clear scores button and set it to clearScoresBtn
 const clearScoresBtn = document.querySelector("#clearScoresBtn");
+
+// Add event listener to nxt1Btn button
+clearScoresBtn.addEventListener("click", clearScores);
+
+//! -- FUNCTIONS -- //
 
 
 
